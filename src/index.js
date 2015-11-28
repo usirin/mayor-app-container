6dddafca396f40f9e2f60af9354a4600e7ca6b6c
Umut Sirin <umutsirin1@gmail.com>
2015-11-27 22:45:59 -0800

    Add initial version of AppContainer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,74 @@
-export default {
+import assign from 'lodash.assign'
+import invariant from 'invariant'
+import Container from './Container'
+import { isServiceProvider } from './ServiceProvider'
+
+export default class AppContainer extends Container {
+
+  constructor() {
+    super()
+    this.serviceProviders = {}
+  }
+
+  /**
+   * Register a provider.
+   *
+   * @public
+   * @param {object} provider
+   */
+  register(ProviderClass) {
+
+    invariant(
+      isServiceProvider(ProviderClass),
+      'AppContainer#register: expected provider to be a ServiceProvider, got %s',
+      typeof ProviderClass
+    )
+
+    let provider = this.resolveProviderClass(ProviderClass)
+
+    provider.register()
+
+    this.markAsRegistered(provider)
+  }
+
+  /**
+   * Resolve a provider class.
+   *
+   * @public
+   * @param {class} ServiceProviderClass
+   * @return {object}
+   */
+  resolveProviderClass(ServiceProviderClass) {
+    return new ServiceProviderClass(this)
+  }
+
+  /**
+   * Return names of registered service providers.
+   *
+   * @public
+   * @return {array}
+   */
+  getProviderNames() {
+    let providers = this.serviceProviders
+
+    return Object.keys(providers).map(key => {
+      return providers[key].getName()
+    })
+  }
+
+  /**
+   * Mark provider instance as registered.
+   *
+   * @public
+   * @param {object} provider
+   * @return {object} registered service providers
+   */
+  markAsRegistered(provider) {
+    this.serviceProviders = assign({}, this.serviceProviders, {
+      [provider.getName()]: provider
+    })
+
+    return this.serviceProviders
+  }
 }
+
--- test/index.js
+++ test/index.js
@@ -1,8 +1,110 @@
 import expect from 'expect'
 import { Reactor } from 'nuclear-js'
 
-describe('test me', () => {
+import AppContainer from '../src'
+import ServiceProvider from '../src/ServiceProvider'
+
+let uid = () => `${Date.now()}`
+
+let createServiceProvider = (config) => {
+  class _ServiceProvider extends ServiceProvider {
+    constructor(app) {
+      super(app)
+      Object.keys(config).forEach((key) => {
+        if (key !== 'init') this[key] = config[key]
+      })
+      if (config.init) {
+        config.init(app)
+      }
+
+      this.name = uid()
+    }
+    getName() {
+      return this.name
+    }
+  }
+
+  return _ServiceProvider
+}
+
+
+describe('AppContainer', () => {
   it('works', () => {
-    expect(true).toExist()
+    expect(new AppContainer).toExist()
+  })
+
+  it("registers services providers correctly", () => {
+    let app = new AppContainer
+
+    // app.register()
+  })
+
+  describe('#register', () => {
+
+
+    it("throws if given provider is not a class", () => {
+      let container = new AppContainer
+
+      expect(() => {
+        container.register({})
+      }).toThrow(/expected provider to be a ServiceProvider/)
+    })
+
+    it("calls given service provider's register method", () => {
+      let flag = false
+      let ServiceProvider = createServiceProvider({
+        register() {
+          flag = true
+        }
+      })
+
+      let container = new AppContainer
+      container.register(ServiceProvider)
+
+      expect(flag).toBe(true)
+    })
+
+    it("initializes given service provider if it's a class", () => {
+      let flag = false
+      let ServiceProvider = createServiceProvider({
+        register() {
+          flag = true
+        }
+      })
+
+      let container = new AppContainer
+      container.register(ServiceProvider)
+
+      expect(flag).toBe(true)
+    })
+
+    it("injects app container to given service provider class", () => {
+      let _app = null
+      let ServiceProvider = createServiceProvider({
+        init(app) {
+          _app = app
+        }
+      })
+
+      let container = new AppContainer
+      container.register(ServiceProvider)
+
+      expect(_app).toBe(container)
+    })
+
+    it("marks given provider as registered", () => {
+      let ServiceProvider = createServiceProvider({
+        getName() { return 'MockServiceProvider' }
+      })
+
+      let container = new AppContainer
+      container.register(ServiceProvider)
+
+      expect(
+        container.getProviderNames()
+      ).toInclude('MockServiceProvider')
+    })
+
   })
 })
+
